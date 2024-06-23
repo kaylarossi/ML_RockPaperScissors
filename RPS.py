c00d4d595a414108ef8f6034a051d0df7fe5c15a
Kayla Rossi <kmr024@bucknell.edu>
2024-06-23 14:57:38 +0000

    feat:added first player stragety

--- RPS.py
+++ RPS.py
@@ -1,10 +1,19 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
 def player(prev_play, opponent_history=[]):
+    
     opponent_history.append(prev_play)
 
-    guess = "R"
+    guess = "S"
     if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+        #guess = opponent_history[-2]
+        #cycle through R,P,S
+        if opponent_history[-1] == "R":
+            guess = "P"
+        elif opponent_history[-1] == "P":
+            guess = "S"
+        else:
+            guess = "R"
+
 
     return guess
